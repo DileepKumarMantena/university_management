e0f21ffa7fdc4c8ecccfdcef62bdc66931e5f790
DileepKumarMantena <deelipkumar261997@gmail.com>
2024-06-15 10:41:34 +0530

    Commited basic structure for projectapp

---
+++ university_management_project/university_management_app/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class university_management_appConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'university_management_app'
